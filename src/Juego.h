411fc4ce84bcba0efebce96683dacc9046c25d12
walterfl1 <00111925@uca.edu.sv~>
2025-07-06 15:23:50 -0600

    CambiosMapasV1.0

--- src/Juego.h
+++ src/Juego.h
@@ -8,70 +8,22 @@
 #include "Minijuegos.h"
 #include "Combate.h"
 #include "Jugador.h"
-#include "Archivos.h"
 using namespace std;
 
-const int FILAS = 12;
-const int COLUMNAS = 20;
-char mapa[FILAS][COLUMNAS];
+const int FILAS = 7;
+const int COLUMNAS = 7;
+char mapa[FILAS][COLUMNAS] = {
+    {'.', '.', 'E', '.', '.', 'J', '.'},
+    {'.', 'X', '.', '.', 'X', '.', '.'},
+    {'P', '.', '.', 'T', '.', '.', 'E'},
+    {'.', '.', '.', '.', '.', '.', '.'},
+    {'E', '.', 'X', '.', '.', '.', 'J'},
+    {'.', '.', '.', '.', 'T', '.', '.'},
+    {'.', 'J', '.', 'E', '.', 'X', '.'}
+};
 
-int jugadorFila = 1;
-int jugadorColumna = 1;
-
-void inicializarMapa() {
-    // Bordes y paredes internas
-    for (int i = 0; i < FILAS; ++i)
-        for (int j = 0; j < COLUMNAS; ++j)
-            mapa[i][j] = (i==0 || i==FILAS-1 || j==0 || j==COLUMNAS-1) ? '#' : '.';
-    for (int i = 3; i < 9; ++i) mapa[i][5] = '#';
-    for (int j = 5; j < 16; ++j) mapa[4][j] = '#';
-    // Elementos
-    mapa[jugadorFila][jugadorColumna] = 'P';
-    mapa[6][17] = 'J';
-    mapa[2][10] = 'T';
-    mapa[10][15] = 'T';
-    mapa[3][16] = 'E';
-    mapa[7][3] = 'E';
-    mapa[6][7] = 'X';
-}
-
-void mostrarMapaBonito() {
-    cout << "   ╔";
-    for (int c = 0; c < COLUMNAS; ++c) cout << "═══";
-    cout << "╗\n";
-    for (int i = 0; i < FILAS; ++i) {
-        cout << (i % 10) << " ║";
-        for (int j = 0; j < COLUMNAS; ++j) {
-            if (mapa[i][j] == '#')
-                cout << "███";
-            else if (mapa[i][j] == '.')
-                cout << " · ";
-            else if (mapa[i][j] == 'P')
-                cout << " @ ";
-            else if (mapa[i][j] == 'J')
-                cout << " ☠ ";
-            else if (mapa[i][j] == 'E')
-                cout << " $ ";
-            else if (mapa[i][j] == 'T')
-                cout << " ? ";
-            else if (mapa[i][j] == 'X')
-                cout << " X ";
-            else
-                cout << "   ";
-        }
-        cout << "║\n";
-    }
-    cout << "   ╚";
-    for (int c = 0; c < COLUMNAS; ++c) cout << "═══";
-    cout << "╝\n";
-    static bool leyenda = true;
-    if (leyenda) {
-        cout << "\n--- LEYENDA ---\n";
-        cout << "@=Tú  ☠=Jefe  $=Ciudad  ?=Minijuego  X=Obstáculo  ·=Camino  █=Pared\n";
-        cout << "----------------\n";
-        leyenda = false;
-    }
-}
+int jugadorFila = 2;
+int jugadorColumna = 0;
 
 void interactuar(char tipoCasilla) {
     switch (tipoCasilla) {
@@ -96,6 +48,18 @@ void interactuar(char tipoCasilla) {
     }
 }
 
+void mostrarMapa() {
+    system("cls"); // Cambia por "clear" en Linux/Mac
+    cout << "\n--- Mapa de Secret Con+inen+ ---\n";
+    for (int i = 0; i < FILAS; ++i) {
+        for (int j = 0; j < COLUMNAS; ++j) {
+            cout << mapa[i][j] << "  ";
+        }
+        cout << endl;
+    }
+    cout << "\nUsa W/A/S/D para moverte, Q para salir\n";
+}
+
 bool moverJugador(char direccion) {
     int nuevaFila = jugadorFila;
     int nuevaColumna = jugadorColumna;
@@ -113,8 +77,8 @@ bool moverJugador(char direccion) {
         return false;
     }
 
-    if (mapa[nuevaFila][nuevaColumna] == '#' || mapa[nuevaFila][nuevaColumna] == 'X') {
-        cout << "\nNo puedes pasar, hay una pared o un obstáculo!\n";
+    if (mapa[nuevaFila][nuevaColumna] == 'X') {
+        cout << "\nHay un obstáculo!\n";
         return false;
     }
 
@@ -130,12 +94,11 @@ bool moverJugador(char direccion) {
 
 void iniciarJuego() {
     srand(time(0));
-    inicializarMapa();
     crearJugador(jugadorFila, jugadorColumna);
 
     char opcion;
     do {
-        mostrarMapaBonito();
+        mostrarMapa();
         mostrarEstado();
         cout << "\nMovimiento: ";
         cin >> opcion;
@@ -143,7 +106,7 @@ void iniciarJuego() {
         moverJugador(opcion);
     } while (true);
 
-    cout << "\n¡Gracias por jugar Secret Con+inen+!\n";
+    cout << "\nGracias por jugar Secret Con+inen+!\n";
 }
 
 #endif
